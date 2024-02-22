43b288c3b4967560bfb4357f4fe9e73fec825891
sherryzhang1457 <159584305+sherryzhang1457@users.noreply.github.com>
2024-02-22 10:51:05 -0800

    Update app.py

--- app.py
+++ app.py
@@ -94,14 +94,12 @@ if submit:
     
     with st.container():
         for index, result in results.iterrows():
-            job_info = result['job_title'] + ', ' + result['job_location']
+            job_info = result['job_title'] + ' | ' + result['job_location'] + ' | ' + result['company'] 
             with st.expander(job_info):
-                st.write('**Company:** ' + result['company'])
-
                 st.markdown('**Job Description**')
                 st.write(result['job_summary'])
-                st.link_button("Apply it!", result["job_link"])
-                st.page_link(result["job_link"], label="**Apply it!**")
+                st.link_button("Apply it!", result["job_link"], type="primary")
+                # st.page_link(result["job_link"], label="**Apply it!**")
 
                 response=get_gemini_response(input_prompt_resume1,resume,result['job_summary'])
                 st.subheader("Disqualifications")
