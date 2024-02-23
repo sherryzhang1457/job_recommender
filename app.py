e0a94f7df28f799018d7bc59dfe1d33bce59977b
sherryzhang1457 <159584305+sherryzhang1457@users.noreply.github.com>
2024-02-23 00:02:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ job_postings = job_postings.fillna('')
 
 # Find the most relevant job description and return the job posting information 
 def get_relevant_ids(query, db, count, df):
-    result = db.query(query_texts=[query], n_results=3, include = ["distances"])
+    result = db.query(query_texts=[query], n_results=count, include = ["distances"])
     ids = result['ids'][0]
     score = result['distances'][0]
     return df.iloc[ids], score
