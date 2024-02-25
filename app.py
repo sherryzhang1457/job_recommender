71dd0c7dc3d7ce756b5859af180f8d9beb955a7d
sherryzhang1457 <159584305+sherryzhang1457@users.noreply.github.com>
2024-02-24 22:27:29 -0800

    Update app.py

--- app.py
+++ app.py
@@ -115,17 +115,17 @@ if submit:
                 st.markdown('**Job Description**')
                 st.write(doc[i])
                 st.link_button("Apply it!", meta[i]['link'], type="primary")
-
-                response=get_gemini_response(input_prompt_resume1,resume,doc[i])
-                st.subheader("Disqualifications")
-                st.write(response)        
-
-                response=get_gemini_response(input_prompt_resume2,resume,doc[i])
-                st.subheader("Skills you may want to add")
-                st.write(response)
-
-                response=get_gemini_response(input_prompt_cover_letter,resume,doc[i])
-                st.subheader("Coverletter")
-                st.write(response)
+                if st.toggle('Generate LLM results'):
+                    response=get_gemini_response(input_prompt_resume1,resume,doc[i])
+                    st.subheader("Disqualifications")
+                    st.write(response)        
+    
+                    response=get_gemini_response(input_prompt_resume2,resume,doc[i])
+                    st.subheader("Skills you may want to add")
+                    st.write(response)
+    
+                    response=get_gemini_response(input_prompt_cover_letter,resume,doc[i])
+                    st.subheader("Coverletter")
+                    st.write(response)
                 time.sleep(5)
                 
