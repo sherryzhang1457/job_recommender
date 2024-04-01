2d97014a22baf8aa5fa82fd373de6357db5ad133
sherryzhang1457 <159584305+sherryzhang1457@users.noreply.github.com>
2024-04-01 13:47:19 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import pandas as pd
 import PyPDF2 as pdf
 import streamlit as st
 import google.generativeai as genai
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 import chromadb
 import cohere
 from chromadb.utils import embedding_functions
@@ -24,28 +25,34 @@ def get_gemini_response(input,pdf_content,prompt):
     generation_config = {
         "temperature": 0.0
     }
-    safety_settings = [
-    {
-        "category": "HARM_CATEGORY_DANGEROUS",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HARASSMENT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_HATE_SPEECH",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-        "threshold": "BLOCK_NONE",
-    },
-    {
-        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-        "threshold": "BLOCK_NONE",
-    },
-    ]
+    safety_settings={
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE
+    }
+    # safety_settings = [
+    # {
+    #     "category": "HARM_CATEGORY_DANGEROUS",
+    #     "threshold": "BLOCK_NONE",
+    # },
+    # {
+    #     "category": "HARM_CATEGORY_HARASSMENT",
+    #     "threshold": "BLOCK_NONE",
+    # },
+    # {
+    #     "category": "HARM_CATEGORY_HATE_SPEECH",
+    #     "threshold": "BLOCK_NONE",
+    # },
+    # {
+    #     "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    #     "threshold": "BLOCK_NONE",
+    # },
+    # {
+    #     "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    #     "threshold": "BLOCK_NONE",
+    # },
+    # ]
     model=genai.GenerativeModel('gemini-pro')
 
     if input:
